e86920dd69b6b8c5a45b7b02d1c1702f4f670a05
MB <michael@thefarmonline.com>
2017-11-08 19:37:48 -0600

    unit testing complete

--- diamond.js
+++ diamond.js
@@ -1,37 +1,30 @@
-var s = 'A';
-var sCode = s.charCodeAt(0);
-document.getElementById("out1").innerHTML = sCode;
-document.getElementById("out2").innerHTML = String.fromCharCode(sCode);
-document.getElementById("out3").innerHTML = "+ " + spacesByCode(65) + "+";
 
+function makeDiamond(){
+  var s = document.getElementById("diamondLetter").value;
+  var sCode = s.charCodeAt(0);
 
-//document.getElementById("result").innerHTML = "A</br>B&nbspB</br>C&nbsp&nbsp&nbspC</br>D&nbsp&nbsp&nbsp&nbsp&nbspD</br>E&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspE";
-
-
-var output = [];
-output.push("A</br>");
-
-var numRows = sCode - 65;
-var nextCharCode = 66;
-var i;
-for (i = 0; i < numRows; i++) {
-    var thisChar = nextCharCode + i;
-    output.push(String.fromCharCode(thisChar) + spacesByCode(thisChar) + String.fromCharCode(thisChar) + "<br/>");
-}
-
-// output now has A through requested. Now flip it.
-
-for (i = output.length - 2; i >= 0; i--) {
-  output.push(output[i]);
-}
-
-// if there's only row, put another A in there so is A-A not just A
-if (output.length == 1) {
+  var output = [];
   output.push("A</br>");
+
+  var numRows = sCode - 65;
+  var nextCharCode = 66;
+  var i;
+  for (i = 0; i < numRows; i++) {
+      var thisChar = nextCharCode + i;
+      output.push(String.fromCharCode(thisChar) + spacesByCode(thisChar) + String.fromCharCode(thisChar) + "<br/>");
+  }
+
+  // output now has A through requested. Now flip it.
+  for (i = output.length - 2; i >= 0; i--) {
+    output.push(output[i]);
+  }
+
+  // if there's only row, put another A in there so is A-A not just A
+  if (output.length == 1) {
+    output.push("A</br>");
+  }
+  document.getElementById("result").innerHTML = output.join("");
 }
-document.getElementById("result").innerHTML = output.join("");
-
-
 
 function spacesByCode(code) {
   var spaces = "";
